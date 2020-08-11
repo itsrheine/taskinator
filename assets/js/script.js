e4c980bca20185fcb0ece2d6f1d0e4b08bfe6018
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-10 17:35:55 -0700

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,32 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskTypeInput);
+
+    var listItemEl = document.createElement("li");
+    tasksToDoEl.appendChild(listItemEl);
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent= "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.textContent = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + " </span>"; 
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
+// formEl.addEventListener("submit", taskFormHandler);
 formEl.addEventListener("submit", createTaskHandler);
+
 

dce1710fe129da1c8505cd1ccf969583a7dfecd3
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-12 20:41:13 -0700

    attach task item in drop zone

--- assets/js/script.js
+++ assets/js/script.js
@@ -216,7 +216,31 @@ var dropZoneDragHandler = function(event) {
     }
 };
 
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+
+    if (statusType === "tasks-todo") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    }
+
+    dropZoneEl.appendChild(draggableElement);
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+pageContentEl.addEventListener("drop", dropTaskHandler);
